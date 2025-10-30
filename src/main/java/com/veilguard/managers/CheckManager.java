ed7ca5a49af7e759093d14645636e6232e90796d
THEMPGUY <realmsnetwork4@gmail.com>
2025-10-30 14:18:55 -0400

    Create CheckManager.java

---
+++ src/main/java/com/veilguard/managers/CheckManager.java
@@ -0,0 +1,36 @@
+package com.veilguard.managers;
+
+import com.veilguard.VeilGuard;
+import com.veilguard.checks.Check;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CheckManager {
+
+    private final VeilGuard plugin;
+    private final List<Check> checks = new ArrayList<>();
+
+    public CheckManager(VeilGuard plugin) {
+        this.plugin = plugin;
+    }
+
+    public void registerCheck(Check check) {
+        checks.add(check);
+        check.onEnable();
+    }
+
+    public void unregisterCheck(Check check) {
+        checks.remove(check);
+        check.onDisable();
+    }
+
+    public void runChecks(Player player, Object... args) {
+        for (Check check : checks) {
+            if (check.isEnabled()) {
+                check.onPlayerAction(player, args);
+            }
+        }
+    }
+}
