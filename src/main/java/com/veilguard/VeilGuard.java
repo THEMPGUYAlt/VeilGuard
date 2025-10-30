bb866de64c5a49e60f72cda6bd0295a0db207a8d
THEMPGUY <realmsnetwork4@gmail.com>
2025-10-30 14:16:46 -0400

    Create VeilGuard.java

---
+++ src/main/java/com/veilguard/VeilGuard.java
@@ -0,0 +1,56 @@
+package com.veilguard;
+
+import com.veilguard.managers.CheckManager;
+import com.veilguard.managers.AlertManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class VeilGuard extends JavaPlugin {
+
+    private static VeilGuard instance;
+    private CheckManager checkManager;
+    private AlertManager alertManager;
+    private boolean protocolLibAvailable = false;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+
+        // Detect ProtocolLib
+        protocolLibAvailable = getServer().getPluginManager().getPlugin("ProtocolLib") != null;
+        if (protocolLibAvailable) {
+            getLogger().info("ProtocolLib detected! Advanced packet checks enabled.");
+        } else {
+            getLogger().info("ProtocolLib not detected. Some checks will use reflection.");
+        }
+
+        alertManager = new AlertManager(this);
+        checkManager = new CheckManager(this);
+
+        checkManager.registerCheck(new com.veilguard.checks.FakeOreBaitCheck());
+        checkManager.registerCheck(new com.veilguard.checks.MineSightCheck());
+
+        getLogger().info("VeilGuard has been enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("VeilGuard has been disabled.");
+    }
+
+    public static VeilGuard getInstance() {
+        return instance;
+    }
+
+    public CheckManager getCheckManager() {
+        return checkManager;
+    }
+
+    public AlertManager getAlertManager() {
+        return alertManager;
+    }
+
+    public boolean isProtocolLibAvailable() {
+        return protocolLibAvailable;
+    }
+}
